f9f940625a49cae954c8c88e798606fa8804fb7a
soma_arc <somaarcr@gmail.com>
2017-11-04 23:29:04 +0900

    Add new build environment

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+
+window.addEventListener('load', () => {
+    /* eslint-disable no-new */
+    // const app = new Vue({
+    //     el: '#app',
+    //     data: d,
+    //     render: (h) => {
+    //         return h('root', { 'props': d })
+    //     },
+    //     components: { 'root': Root }
+    // })
+});
