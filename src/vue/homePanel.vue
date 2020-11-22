5528b2aeb01bb010affd17799d9281d12acb6634
soma_arc <somaarcr@gmail.com>
2020-11-22 15:22:23 +0900

    Update project

--- src/vue/homePanel.vue
+++ src/vue/homePanel.vue
@@ -18,12 +18,12 @@
     </div>
     <h1>Contacts</h1>
     <ul class="about-links">
+      <li><a href="https://twitter.com/soma_arc" target="_blank">Twitter</a></li>
       <li><a href="https://github.com/soma-arc" target="_blank">GitHub</a></li>
-      <li><a href="https://www.facebook.com/kento.nakamura.581" target="_blank">Facebook</a></li>
       <li><a href="https://www.shadertoy.com/user/soma_arc" target="_blank">Shadertoy</a></li>
       <li><a href="http://soma.hatenablog.jp/" target="_blank">Hatena Blog</a></li>
       <li><a href="https://www.youtube.com/channel/UCZPcM9n56N-ftmJfHKoC03Q" target="_blank">YouTube</a></li>
-      <li><a href="https://twitter.com/soma_arc" target="_blank">Twitter</a></li>
+      <li><a href="https://www.facebook.com/kento.nakamura.581" target="_blank">Facebook</a></li>
     </ul>
   </div>
 </template>
